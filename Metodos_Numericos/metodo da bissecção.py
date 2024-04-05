8ef795c9f00aa64963d45129a6dfd9cfc0c65741
Julio-Cesar-RM <jcr.montagnani@unesp.br>
2024-04-05 20:47:06 -0300

    alterada função do método da bissecção

--- Metodos_Numericos/metodo da bissecção.py
+++ Metodos_Numericos/metodo da bissecção.py
@@ -2,8 +2,8 @@ from defs import modulo
 a = float(input('Entre com o a: '))
 b = float(input('Entre com o valor de b: '))
 error = float(input('Entre com o erro: '))
-fa = a**2 + a - 6
-fb = b**2 + b - 6
+fa = a**3 - 3*(a**2) + 1
+fb = b**3 - 3*(b**2) + 1
 k = 0
 if fa*fb < 0:
     print('Existe uma raiz entre a e b.')
@@ -11,9 +11,9 @@ else:
     print('Não existe uma raiz entre a e b, tente novamente.')
 while True:
     k = k + 1
-    print(f'k = {k} /// a = {a} /// b = {b} /// f(a) = {fa} /// f(b) = {fb} /// Erro = {error}')
-    c = (a+b)/2
-    fc = c**2 + c - 6
+    c = (a+b)/2  
+    fc = c**3 - 3*(c**2) + 1
+    print(f'k = {k} / a = {a} / b = {b} / c = {c} / f(a) = {fa} / f(b) = {fb} / f(c) = {fc} / Erro = {error}')  
     if modulo(fc) < error:
         print(f'c é a raiz aproximada, sendo igual a: {c}.')
         break
@@ -21,7 +21,7 @@ while True:
         b = c
     else:
         a = c
-    fa = a**2 + a - 6
-    fb = b**2 + b - 6
+    fa = a**3 - 3*(a**2) + 1
+    fb = b**3 - 3*(b**2) + 1
     input("Pressione Enter para continuar...")
 
